f6e2a312672a0fb00515c965c50525b59228c698
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2018-06-26 04:29:45 -0300

    refactoring
    refactoring

--- rs.py
+++ rs.py
@@ -1,5 +1,10 @@
 # Recommender systems
-# Find similar users, find movies, recommend movies, test the recommendation acurracy
+# 1- Find similar users using KNN and K-means
+# 2- Find useful movies
+# 3- Compute prediction list of values (predicted rank)
+# 4- Use evaluation metrics to analyze the quality of the prediction
+# 5- Plot the chart
+#
 # Lucas cordeiro da Silva
 # UFF - Universidade Federal Fluminense
 
@@ -37,48 +42,46 @@ class RecommenderSystem(object):
     def ClusterIndicesNumpy(self, clustNum, labels_array):  # numpy
         return np.where(labels_array == clustNum)[0]
 
+    # Definir peso de acordo com a distancia
+    def DefineWeight(self, distance):
+        return 1/(distance)**2
+
+    # Distancia Euclidiana
+    def euclideanDistance(self, user1, user2):
+        return sqrt(sum(pow(a-b, 2) for a, b in zip(user1, user2)))
+
     # Encontrar os filmes que o usuário ativo já assistiu
     def getWatchedMovies(self, user_vector):  # numpy
-        evaluations = {}
-        # pega filmes assistidos pelo usuário ativo
-        for i, col in enumerate(user_vector):
-            if col > 0:
-                evaluations.setdefault(i, None)
-                evaluations[i] = col
-        return evaluations
+        return dict((i, rating) for i, rating in enumerate(user_vector) if rating > 0)
 
     # Montar matriz usuariossimilares-filmes
     def mountSimilarUsersMatrix(self, matrix, users):
-        users_vector = []
+        return [matrix[user] for user in users]
 
-        for user in users:
-            users_vector.append(matrix[user])
-        return users_vector
-
-    # Gerar recomendacao de notas para filmes
-    def recommendMovies(self, similarusers_matrix, activeuser_whatchedmovies):
+    # Gerar predicao de notas para lista de filmes
+    def predictRank(self, similarusers_matrix, activeuser_whatchedmovies, weights):
 
         # Contador de avaliacoes
         ratings = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
         recommendation = {}
 
-        # Predizer notas aos filmes assistidos
+        # Predizer notas aos filmes
         for movie in activeuser_whatchedmovies:
             ratings = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+            i = 0
             for user in similarusers_matrix:
                 rating = user[movie]
                 if rating > 0:
-                    ratings[rating] += 1
+                    ratings[rating] += (1 * weights[i])
+                i += 1
             recommendation.setdefault(movie, None)
             # Pega a nota com maior ocorrencia , ex: {1: 3, 2: 7, 3: 19, 4: 26, 5: 6} => 4
             recommendation[movie] = max(
                 ratings.items(), key=operator.itemgetter(1))[0]
-            # print(ratings, max(
-            #     ratings.items(), key=operator.itemgetter(1))[0])
 
         return recommendation
 
-    # Avaliar Recomendacao usando Kendall Tau, MAE, RMSE
+    # Avaliar predicao usando Kendall Tau, MAE e RMSE
     def PredictionAccuracy(self, y_true, y_pred):
         tau = stats.kendalltau(y_true, y_pred)[0]
         mae = mean_absolute_error(y_true, y_pred)
@@ -115,13 +118,17 @@ class RecommenderSystem(object):
         similarusers = self.ClusterIndicesNumpy(
             activeuser_clusterlabel, users_clusterlabel)
 
-        print("kmeans: ", similarusers)
-
         # Gerar matrix usuariossimilares-filmes
         similarusers_matrix = self.mountSimilarUsersMatrix(
             matrix, similarusers)
 
-        return similarusers_matrix
+        # criar vetor de distancias
+        distances = [self.euclideanDistance(
+            activeuser_vector, user_vector) for user_vector in similarusers_matrix]
+        # criar vetor de pesos
+        weights = [self.DefineWeight(distance) for distance in distances]
+
+        return similarusers_matrix, weights
 
     # Pegar usuários similares usando KNN
     def getSimilarUsersknn(self, matrix, activeuser_vector):
@@ -129,13 +136,14 @@ class RecommenderSystem(object):
         # Preveja quais sao os K usuarios mais proximos ao usuario ativo
         distances, similarusers = self.nbrs.kneighbors([activeuser_vector])
 
-        print("knn: ", similarusers)
-
         # Gerar matrix usuariossimilares-filmes
         similarusers_matrix = self.mountSimilarUsersMatrix(
             matrix, similarusers[0])
 
-        return similarusers_matrix
+        # criar vetor de pesos
+        weights = [self.DefineWeight(distance) for distance in distances[0]]
+
+        return similarusers_matrix, weights
 
     # Exibe os gráficos
     def renderCharts(self, results, kvalues):
@@ -178,36 +186,37 @@ class RecommenderSystem(object):
         plt.show()
 
 
-#
+# Classe com funcoes para importar o dataset e construir as matrizes para input
 class Data(object):
 
-    # Matrix user-movie
+    # Matriz Geral
     matrix = []
+    # Matriz de treinamento
     matrixtrain = []
+    # Matriz de teste
     matrixtest = []
+    # Tamanho da matriz de treinamento
     trainpercentage = 0.0
 
     def __init__(self, ratings_path, trainpercentage):
         self.trainpercentage = trainpercentage
         self.LoadData(ratings_path)
 
-    # Load dataBase
+    # Carregar Base de dados
     def LoadData(self, ratings_path):
 
-        # File Read
+        # Ler arquivo
         colname = ['user', 'movie', 'rating', 'timestamp']
         df = pd.read_table(ratings_path, sep='\t', header=None,
                            names=colname, encoding="ISO-8859-1")
 
-        # Determine the number of unique users and movies
+        # Determinar o número de usuários e filmes
         num_users = len(np.unique(df[['user']].values))
         num_movies = len(np.unique(df[['movie']].values))
 
-        # Matrix of size num_users x num_movies (rows x cols)
+        # Matriz de tamanho (num_users x num_movies) <=> (linha x coluna) <=> (mxn)
         self.matrix = [[0 for _ in range(num_movies)]
                        for _ in range(num_users)]
-
-        # print(num_users, num_movies)
 
         # Fill in matrix from data
         for index in df.index:
@@ -218,34 +227,19 @@ class Data(object):
 
         last = int(round(num_users * self.trainpercentage))
 
-        self.matrixtrain = self.matrix[:last]
+        self.matrixtrain = self.matrix[: last]
         self.matrixtest = self.matrix[last:]
 
 
 # Inicia tudo
 def main():
-
-    # trainpercentage = 0.8
-    # data = Data("100k/u.data", trainpercentage)
-    # # matrix numpy
-    # X = np.array(data.matrixtrain)
-    # k = 3
-    # nbrs = NearestNeighbors(n_neighbors=k)
-
-    # nbrs.fit(X)
-
-    # nbrs.fit([[1, 2, 3, 4, 5], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2]])
-
-    # distances, indices = nbrs.kneighbors([[1, 1, 1, 1, 1]])
-    # print(indices[0])
-    # sys.exit()
-
-    # K-Means Method -------------------------------------------
-
     # Parametros de funcionamento do K
     k_min = 3
     k_max = 13
     step = 1
+
+    # Lista de K a serem estudados
+    kvalues = [k for k in range(k_min, (k_max + step), step)]
 
     # Tamanho da matrix de treinamento, ex: 0.5 -> 50% treinamento e 50% teste
     trainpercentage = 0.70
@@ -270,10 +264,9 @@ def main():
     RecommenderSystemd = RecommenderSystem(X)
 
     # para cada valor de K
-    kvalues = [k for k in range(k_min, (k_max + step), step)]
-    for k in range(k_min, (k_max + step), step):
+    for k in kvalues:
 
-        # Metricas de avaliacao da recomendacao
+        # Metricas de avaliacao
         metrics = {"kmeans": {"tau": [], "mae": [], "rmse": []},
                    "knn": {"tau": [], "mae": [], "rmse": []}}
 
@@ -293,21 +286,21 @@ def main():
 
             # 3- Encontrar usuários similares ao usuário ativo
             # K-Means
-            similarusers_matrix = RecommenderSystemd.getSimilarUserskmeans(
+            similarusers_matrix, weightskmeans = RecommenderSystemd.getSimilarUserskmeans(
                 data.matrixtrain, activeuser_vector)
 
             # KNN
-            similarusers_matrixknn = RecommenderSystemd.getSimilarUsersknn(
+            similarusers_matrixknn, weightsknn = RecommenderSystemd.getSimilarUsersknn(
                 data.matrixtrain, activeuser_vector)
 
-            # 4- Recomendar filmes dos usuários similares ao usuário ativo
+            # 4- Predizer notas para lista de filmes
             # K-Means
-            recommendation = RecommenderSystemd.recommendMovies(
-                similarusers_matrix, activeuser_whatchedmovies)
+            recommendation = RecommenderSystemd.predictRank(
+                similarusers_matrix, activeuser_whatchedmovies, weightskmeans)
 
             # KNN
-            recommendationknn = RecommenderSystemd.recommendMovies(
-                similarusers_matrixknn, activeuser_whatchedmovies)
+            recommendationknn = RecommenderSystemd.predictRank(
+                similarusers_matrixknn, activeuser_whatchedmovies, weightsknn)
 
             # 5- Calcular Precisao da previsão
             # K-Means
@@ -325,8 +318,6 @@ def main():
             metrics["knn"]["tau"].append(tauknn)
             metrics["knn"]["mae"].append(maeknn)
             metrics["knn"]["rmse"].append(rmseknn)
-            # print("Predicted Ratings: ", recommendation, "\n")
-            # print("Real ratings: ", activeuser_whatchedmovies, "\n")
 
         # Encontrar a média dos calculos de precisao e guardar
         metrics["kmeans"]["tau"] = sum(
